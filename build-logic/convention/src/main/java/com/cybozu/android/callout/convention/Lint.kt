5f357d6567c326cd9f232ffa7b8856ae75a14367
shunm-999 <shun-miyazaki@cybozu.co.jp>
2025-02-25 15:03:12 +0900

    Add Apache License 2.0 comment

--- build-logic/convention/src/main/java/com/cybozu/android/callout/convention/Lint.kt
+++ build-logic/convention/src/main/java/com/cybozu/android/callout/convention/Lint.kt
@@ -13,14 +13,17 @@ fun Project.configureLint() {
             target("**/*.kt")
             targetExclude("**/build/**/*.kt")
             ktlint(libs.getVersion("ktlint"))
+            licenseHeaderFile(rootProject.file("spotless/copyright.kt"))
         }
         format("kts") {
             target("**/*.kts")
             targetExclude("**/build/**/*.kts")
+            licenseHeaderFile(rootProject.file("spotless/copyright.kts"), "(^(?![\\/ ]\\*).*$)")
         }
         format("xml") {
             target("**/*.xml")
             targetExclude("**/build/**/*.xml")
+            licenseHeaderFile(rootProject.file("spotless/copyright.xml"), "(<[^!?])")
         }
     }
 }
